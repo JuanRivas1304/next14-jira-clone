94f4ad4078ddc5113fb6315b71ba6d8229192c4f
unknown <nixfe3826@gmail.com>
2024-10-23 14:42:59 -0500

    parte1 primeras 5 horas

---
+++ src/components/query-provider.tsx
@@ -0,0 +1,39 @@
+"use client"
+
+import {
+    isServer,
+    QueryClient,
+    QueryClientProvider,
+} from "@tanstack/react-query"
+
+function makeQueryClient(){
+    return new  QueryClient({
+        defaultOptions: {
+            queries: {
+            staleTime: 60 * 1000,
+            },
+        },
+    })
+}
+
+let browserQueryClient: QueryClient | undefined = undefined
+
+function getQueryClient(){
+    if (isServer) {
+        return makeQueryClient()
+    }else{
+        if (!browserQueryClient) browserQueryClient = makeQueryClient()
+        return browserQueryClient
+    }
+}
+
+interface QueryProviderProps {
+    children: React.ReactNode;
+};
+
+export const QueryProvider = ({ children }: QueryProviderProps) => {
+    const QueryClient = getQueryClient()
+    return(
+        <QueryClientProvider client={QueryClient}>{children}</QueryClientProvider>
+    )
+}
---
+++ src/components/sidebar.tsx
@@ -0,0 +1,9 @@
+import { Navigation } from "./navigation"
+
+export const Siderbar = () => {
+    return (
+        <aside className="h-full bg-neutral-100 p-4 w-full">
+            <Navigation />
+        </aside>
+    )
+}
